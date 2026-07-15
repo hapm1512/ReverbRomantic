caeaae0077bba488d9b7ff8bc744b92325b6c068
hapm1512 <cty.haichung@gmail.com>
2026-07-15 13:40:29 +0700

    RC2: finalize GUI cleanup, decay response and CPU optimization

--- Source/DSP/HybridFDN16.h
+++ Source/DSP/HybridFDN16.h
@@ -92,6 +92,7 @@ private:
     };
 
     static RoomProfile getRoomProfile (RoomModel model) noexcept;
+    static bool parametersEqual (const Parameters&, const Parameters&) noexcept;
     void updateFDNCoefficients() noexcept;
     float processHighPass (float input, float& inputState, float& outputState) noexcept;
     float processLowPass (float input, float& state) noexcept;
@@ -123,6 +124,7 @@ private:
     };
 
     Parameters parameters;
+    bool parametersInitialised = false;
     RoomProfile roomProfile { getRoomProfile (RoomModel::hall) };
     double sampleRate = 44100.0;
     float sizeScale = 1.0f;
